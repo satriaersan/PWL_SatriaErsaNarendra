b62e6944cc84b96e1d3ef39469377251a6849727
satriaersan <satriaersanarendra@gmail.com>
2025-03-07 04:23:57 +0700

    Praktikum-4 _jobsheet3

--- minggu03/PWL_POS/routes/web.php
+++ minggu03/PWL_POS/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\LevelController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/level', [LevelController::class, 'index']);
