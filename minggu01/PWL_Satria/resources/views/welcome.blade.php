5ee4ba48ca6ac48a2b3b2fa3f44884b6b9e53a99
satriaersan <satriaersanarendra@gmail.com>
2025-03-06 13:02:18 +0700

    merapikan jobsheet1

---
+++ minggu01/PWL_Satria/resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html> lang="{{ str_replace('_', '-', app()->getlocale()) }}"
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>PWL 2024</title>
+    </head>
+    <body>
+        <h2>Pemrograman Web Lanjut 2024</h2>
+        <h2>Semangat Belajar Laravel 10</h2>
+    </body>
+</html>
