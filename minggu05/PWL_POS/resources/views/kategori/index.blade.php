58babbc3dde9ce1fe395604262297f35b2444d40
satriaersan <satriaersanarendra@gmail.com>
2025-03-20 05:10:22 +0700

    tugas1_jobsheet5

--- minggu05/PWL_POS/resources/views/kategori/index.blade.php
+++ minggu05/PWL_POS/resources/views/kategori/index.blade.php
@@ -1,21 +1,28 @@
 @extends('layout.app')
- 
- {{-- Customize layout sections --}}
- @section('subtitle', 'Kategori')
- @section('content_header_title', 'Home')
- @section('content_header_subtitle', 'Kategori')
- 
- @section('content')
-     <div class="container">
-         <div class="card">
-             <div class="card-header">Manage Kategori</div>
-             <div class="card-body">
-                 {{ $dataTable->table() }}
-             </div>
-         </div>
-     </div>
- @endsection
- 
- @push('scripts')
-     {{ $dataTable->scripts() }}
- @endpush
+
+{{-- Customize layout sections --}}
+@section('subtitle', 'Kategori')
+@section('content_header_title', 'Home')
+@section('content_header_subtitle', 'Kategori')
+
+@section('content')
+    <div class="container">
+        <div class="card">
+            <div class="card-header">
+                <h1>Manage Kategori</h1>
+            </div>
+
+            <div class="card-body">
+                <div class="d-flex justify-content-start">
+                    <a href="{{ route('kategori.create') }}" class="btn btn-primary btn-sm my-3 px-4 py-2 rounded-pill shadow-sm d-flex align-items-center">
+                        <i class="fas fa-plus me-2"></i> <strong>Tambah</strong>
+                    </a>
+                </div>
+                {{ $dataTable->table() }}
+            </div>
+        </div>
+    </div>
+@endsection
+
+@push('scripts')
+    {{ $dataTable->scripts() }}
--- minggu05/PWL_POS/routes/web.php
+++ minggu05/PWL_POS/routes/web.php
@@ -29,5 +29,6 @@ Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
 Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
 Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
 Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
-Route::get('/kategori/create', [KategoriController::class, 'create']);
+// Route::get('/kategori/create', [KategoriController::class, 'create']);
 Route::post('/kategori', [KategoriController::class, 'store']);
+Route::get('/kategori/create', [KategoriController::class, 'create'])->name('kategori.create');
