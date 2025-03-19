035f0a09210f246203475e74e311db9dfea59023
satriaersan <satriaersanarendra@gmail.com>
2025-03-20 01:24:15 +0700

    praktikum3_jobsheet5

--- minggu05/PWL_POS/app/Http/Controllers/KategoriController.php
+++ minggu05/PWL_POS/app/Http/Controllers/KategoriController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\DataTables\KategoriDataTable;
+use App\Models\KategoriModel;
 
 class KategoriController extends Controller
 {
@@ -31,5 +32,20 @@ class KategoriController extends Controller
   public function index(KategoriDataTable $dataTable)
   {
     return $dataTable->render('kategori.index');
+  }
+
+  public function create()
+  {
+    return view('kategori.create');
+  }
+
+  public function store(Request $request)
+  {
+    KategoriModel::create([
+      'kategori_kode' => $request->kodeKategori,
+      'kategori_nama' => $request->namaKategori,
+    ]);
+
+    return redirect('/kategori');
   }
 }
