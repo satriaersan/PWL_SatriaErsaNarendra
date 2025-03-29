a54e80ac32d6650a26b656c51936bca78c7d6410
satriaersan <satriaersanarendra@gmail.com>
2025-03-29 08:46:31 +0700

    Tugas_jobsheet5

--- minggu05baru/PWL_POS/app/Models/LevelModel.php
+++ minggu05baru/PWL_POS/app/Models/LevelModel.php
@@ -9,12 +9,20 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class LevelModel extends Model
 {
-    use HasFactory;
-    public function user():BelongsTo{
+    // use HasFactory;
+    // public function user():BelongsTo{
+    //     return $this->belongsTo(UserModel::class);
+    // }
+    // protected $table = 'm_level';   
+    // protected $primaryKey = 'level_id';  
+
+    protected $table = 'm_level';
+    protected $primaryKey = 'level_id';
+    protected $fillable = ['level_nama', 'level_kode'];
+
+    public function user(): BelongsTo {
+
         return $this->belongsTo(UserModel::class);
     }
-    protected $table = 'm_level';   
-    protected $primaryKey = 'level_id';  
-
    
 }
--- minggu05baru/PWL_POS/routes/web.php
+++ minggu05baru/PWL_POS/routes/web.php
@@ -7,6 +7,10 @@ use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\WelcomeController;
 
+use App\Http\Controllers\BarangController;
+use App\Http\Controllers\SupplierController;
+use Illuminate\Http\Request;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -41,4 +45,47 @@ Route::group(['prefix' => 'user'], function () {
     Route::get('/{id}/edit', [UserController::class, 'edit']);  // menampilkan halaman form edit user
     Route::put('/{id}', [UserController::class, 'update']);     // menyimpan perubahan data user
     Route::delete('/{id}', [UserController::class, 'destroy']); // menghapus data user
+});
+
+
+Route::prefix('level')->group(function () {
+    Route::get('/', [LevelController::class, 'index']);
+    Route::post('/list', [LevelController::class, 'list']);
+    Route::get('/create', [LevelController::class, 'create']);
+    Route::post('/', [LevelController::class, 'store']);
+    Route::get('/{id}/edit', [LevelController::class, 'edit']);
+    Route::put('/{id}', [LevelController::class, 'update']);
+    Route::delete('/{id}', [LevelController::class, 'destroy']);
+});
+
+Route::prefix('kategori')->group(function () {
+    Route::get('/', [KategoriController::class, 'index']);
+    Route::post('/list', [KategoriController::class, 'list']);
+    Route::get('/create', [KategoriController::class, 'create']);
+    Route::post('/', [KategoriController::class, 'store']);
+    Route::get('/{id}/edit', [KategoriController::class, 'edit']);
+    Route::put('/{id}', [KategoriController::class, 'update']);
+    Route::delete('/{id}', [KategoriController::class, 'destroy']);
+});
+
+Route::prefix('supplier')->group(function () {
+    Route::get('/', [SupplierController::class, 'index']);
+    Route::post('/list', [SupplierController::class, 'list']);
+    Route::get('/create', [SupplierController::class, 'create']);
+    Route::post('/', [SupplierController::class, 'store']);
+    Route::get('/{id}', [SupplierController::class, 'show']);
+    Route::get('/{id}/edit', [SupplierController::class, 'edit']);
+    Route::put('/{id}', [SupplierController::class, 'update']);
+    Route::delete('/{id}', [SupplierController::class, 'destroy']);
+});
+
+Route::prefix('barang')->group(function () {
+    Route::get('/', [BarangController::class, 'index']);
+    Route::post('/list', [BarangController::class, 'list']);
+    Route::get('/create', [BarangController::class, 'create']);
+    Route::post('/', [BarangController::class, 'store']);
+    Route::get('/{id}', [BarangController::class, 'show']);
+    Route::get('/{id}/edit', [BarangController::class, 'edit']);
+    Route::put('/{id}', [BarangController::class, 'update']);
+    Route::delete('/{id}', [BarangController::class, 'destroy']);
 });
