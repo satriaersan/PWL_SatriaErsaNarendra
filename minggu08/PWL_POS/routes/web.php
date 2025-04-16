852d25d6815010d8fa0cbd427231d912dc273fc2
satriaersan <satriaersanarendra@gmail.com>
2025-04-16 15:20:03 +0700

    praktikum1_jobsheet8

--- minggu08/PWL_POS/routes/web.php
+++ minggu08/PWL_POS/routes/web.php
@@ -262,6 +262,9 @@ Route::middleware(['auth'])->group(function () { // artinya semua route di dalam
         Route::get('/{id}/delete_ajax', [BarangController::class, 'confirm_ajax']);       //menampilkan form confirm delete barang ajax
         Route::delete('/{id}/delete_ajax', [BarangController::class, 'delete_ajax']);     //menghapus data barang ajax
 
+        Route::get('/import', [BarangController::class, 'import']);                     //ajax form upload excel
+        Route::post('/import_ajax', [BarangController::class, 'import_ajax']);          //ajax import excel
+
         Route::delete('/{id}', [BarangController::class, 'destroy']);
     });
 });
